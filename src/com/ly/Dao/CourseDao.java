c3483982c090c38b8372124b318ddd2a0b76792b
aliyun2560158319-rP6oa <li_uyi@163.com>
2023-03-08 17:54:37 +0800

    成绩

--- src/com/ly/Dao/CourseDao.java
+++ src/com/ly/Dao/CourseDao.java
@@ -1,4 +1,46 @@
 package com.ly.Dao;
 
+import com.ly.entity.Course;
+import com.ly.utils.JDBCUtils;
+import com.ly.utils.ParamsUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class CourseDao {
+    //获取所有课程
+    public List<Course> getCouseList(){
+        Connection con = null;
+        PreparedStatement pre = null;
+        ResultSet resultSet = null;
+        List<Course>res = new ArrayList<Course>();
+        try {
+            con = JDBCUtils.getConnection();
+            String sql = "select * from course ";
+            pre = con.prepareStatement(sql);
+            resultSet = pre.executeQuery();
+            while (resultSet.next()) {
+                Integer courseId = resultSet.getInt("course_id");
+                String courseName = resultSet.getString("course_name");
+                String courseType = resultSet.getString("course_type");
+                Course course = new Course();
+                course.setCourseId(courseId);
+                course.setCourseName(courseName);
+                course.setCourseType(courseType);
+                res.add(course);
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.close(resultSet, pre,con );
+        }
+        return res;
+    }
 }
+
--- src/com/ly/Dao/ExamDao.java
+++ src/com/ly/Dao/ExamDao.java
@@ -1,4 +1,49 @@
 package com.ly.Dao;
 
+import com.ly.entity.Exam;
+import com.ly.entity.dto.ScoreDto;
+import com.ly.utils.JDBCUtils;
+import com.ly.utils.ParamsUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExamDao {
+    //学生查成绩
+    public List<ScoreDto> getScoreList(String query) {
+        Connection con = null;
+        PreparedStatement pre = null;
+        ResultSet resultSet = null;
+        List<ScoreDto> res = new ArrayList<ScoreDto>();
+
+        try {
+            con = JDBCUtils.getConnection();
+            String sql = "select * from (exam inner join student inner join course )where stu_id = ? ";
+            pre = con.prepareStatement(sql);
+            resultSet = pre.executeQuery();
+            while (resultSet.next()) {
+                ScoreDto scoreDto = new ScoreDto();
+                scoreDto.setScore(resultSet.getInt("score"));
+                scoreDto.setDate(resultSet.getDate("date"));
+                scoreDto.setCourseName(resultSet.getString("course_name"));
+                scoreDto.setStuId(resultSet.getInt("stu_id"));
+                scoreDto.setStuName(resultSet.getString("stu_name"));
+                scoreDto.setTime(resultSet.getTime("time"));
+                scoreDto.setStuClass(resultSet.getInt("stu_class"));
+                scoreDto.setMajor(resultSet.getString("major"));
+
+                res.add(scoreDto);
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.close(resultSet, pre, con);
+        }
+        return res;
+    }
 }
